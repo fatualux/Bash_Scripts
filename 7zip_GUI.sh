1cd41963b5aa0cb83b0dd2df3039724c8313bbcf
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-25 20:48:04 +0100

    Refactoring

--- 7zip_GUI.sh
+++ 7zip_GUI.sh
@@ -1,46 +1,67 @@
 #!/bin/bash
+# This script can be used to interact with 7zip, a GUI for 7-Zip. (https://www.7-zip.org)
+# It depends on: bash zenity 7z
 
 function display_error() {
-  yad --error --text="$1"
+  zenity --error --text="$1"
   exit 1
 }
+
+function check_dependencies() {
+  local dependencies=("zenity" "7z")
+
+  for dep in "${dependencies[@]}"; do
+    command -v "$dep" &> /dev/null || display_error "Error: $dep is not installed. Please install it before running this script."
+  done
+}
+
+# Check dependencies before proceeding
+check_dependencies
 
 # Set the working directory to the user's home
 cd "$HOME" || display_error "Could not change directory to home"
 
-action=$(yad --list --title="7zip GUI" --column="Action" "Compress" "Extract")
+# Get the action with trailing '|', and remove it
+action=$(zenity --list --title="7zip GUI" --column="Action" "Compress" "Extract" | sed 's/|$//')
+echo "Debug: \$action is '$action'"
 
-if [ "$action" == "Compress" ]; then
-  archive_operation=$(yad --list --title="Archive Operation" --column="Operation" "Create New Archive" "Add to Existing Archive")
+case "$action" in
+  "Compress")
+    archive_operation=$(zenity --list --title="Archive Operation" --column="Operation" "Create New Archive" "Add to Existing Archive")
+    echo "Debug: \$archive_operation is '$archive_operation'"
 
-  if [ "$archive_operation" == "Create New Archive" ]; then
-    archive_name=$(yad --entry --title="Archive Name")
-    compression_method=$(yad --list --title="Compression Method" --column="Method" "7z" "zip" "tar" "tar.gz" "tar.bz2")
-    compression_level=$(yad --scale --title="Compression Level" --min-value=1 --max-value=9 --value=5 --step=1)
-    file_names=$(yad --file-selection --multiple --title="Select Files")
+    case "$archive_operation" in
+      "Create New Archive")
+        archive_name=$(zenity --entry --title="Archive Name")
+        compression_method=$(zenity --list --title="Compression Method" --column="Method" "7z" "zip" "tar" "tar.gz" "tar.bz2")
+        compression_level=$(zenity --scale --title="Compression Level" --min-value=1 --max-value=9 --value=5 --step=1)
+        file_names=$(zenity --file-selection --multiple --title="Select Files")
 
-    # Compress the files
-    7z a -t${compression_method} -mx${compression_level} "${archive_name}" ${file_names//|/ } || display_error "Compression failed"
+        # Compress the files
+        7z a -t${compression_method} -mx${compression_level} "${archive_name}" ${file_names//|/ } || display_error "Compression failed"
+        ;;
+      "Add to Existing Archive")
+        archive_name=$(zenity --file-selection --title="Select Existing Archive")
+        add_file_names=$(zenity --file-selection --multiple --title="Select Files to Add")
 
-  elif [ "$archive_operation" == "Add to Existing Archive" ]; then
-    archive_name=$(yad --file-selection --title="Select Existing Archive")
-    add_file_names=$(yad --file-selection --multiple --title="Select Files to Add")
+        # Add files to the existing archive
+        7z u "${archive_name}" ${add_file_names//|/ } || display_error "Adding files failed"
+        ;;
+      *)
+        display_error "Invalid Archive Operation"
+        ;;
+    esac
+    ;;
+  "Extract")
+    archive_name=$(zenity --file-selection --title="Select Archive to Extract")
+    output_directory=$(zenity --file-selection --directory --title="Select Output Directory")
 
-    # Add files to the existing archive
-    7z u "${archive_name}" ${add_file_names//|/ } || display_error "Adding files failed"
-  else
-    display_error "Invalid Archive Operation"
-  fi
+    # Extract the files
+    7z x "${archive_name}" -o"${output_directory}" || display_error "Extraction failed"
+    ;;
+  *)
+    display_error "Invalid Action"
+    ;;
+esac
 
-elif [ "$action" == "Extract" ]; then
-  archive_name=$(yad --file-selection --title="Select Archive to Extract")
-  output_directory=$(yad --file-selection --directory --title="Select Output Directory")
-
-  # Extract the files
-  7z x "${archive_name}" -o"${output_directory}" || display_error "Extraction failed"
-
-else
-  display_error "Invalid Action"
-fi
-
-yad --info --text="Operation completed successfully"
+zenity --info --text="Operation completed successfully"
