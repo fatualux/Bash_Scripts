ca4bcd6165e40d535d709b6ecc423892852ff36e
fatualux <35587292+fatualux@users.noreply.github.com>
2024-02-02 12:06:03 +0100

    Removed zenity progress bar and date from titles

--- downloader.sh
+++ downloader.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-# It is a simple bash script to interact with yt-dlp (https://github.com/yt-dlp/yt-dlp)
-# It depends on: bash zenity yt-dlp wl-clipboard
+#!/bin/bash
+# A simple bash script to interact with yt-dlp (https://github.com/yt-dlp/yt-dlp)
+# Depends on: bash, yt-dlp, wl-clipboard
 
 CheckClipboardTool() {
   if command -v wl-copy >/dev/null 2>&1; then
     export CLIP_TOOL="wl-copy"
     export PASTE_TOOL="wl-paste"
   else
-    zenity --error --text "Error: No clipboard tool (wl-copy) found!"
+    echo "Error: No clipboard tool (wl-copy) found!"
     exit 1
   fi
 }
@@ -47,11 +47,10 @@ InsertURL() {
   echo "$URL" >> urls.txt
 }
 
-ProgressFunction() {
-  # Use this function to show progress bar
+DownloadFunction() {
   command="$1"
   shift
-  $command "$@" | zenity --progress --title="Progress" --text="Working..." --percentage=0 --auto-close
+  $command "$@" --print-traffic "$URL" --lazy-playlist
 }
 
 ListActions
@@ -61,38 +60,38 @@ if [ "$ACTION" = "download" ]; then
   echo "VIDEO DOWNLOADER"
   InsertURL
   echo "Searching for available formats..."
-  ProgressFunction yt-dlp -o "Media/$(date +%Y%m%d-%H%M%S)-%(title)s" -i --hls-prefer-ffmpeg --print-traffic "$URL" --lazy-playlist
+  DownloadFunction yt-dlp -o "Media/$(date +%Y%m%d-%H%M%S)-%(title)s" -i --hls-prefer-ffmpeg
   echo "Done!"
   rm urls.txt
-  zenity --info --text="Video Download Completed!"
+  echo "Video Download Completed!"
 fi
 
 if [ "$ACTION" = "extract" ]; then
   echo ""
   echo "AUDIO EXTRACTOR"
   InsertURL
-  ProgressFunction yt-dlp --audio-quality "ba" -x "$URL" -o "Media/$(date +%Y%m%d-%H%M%S)-%(title)s.%(ext)s"
+  DownloadFunction yt-dlp --audio-quality "ba" -x "$URL" -o "Media/$(date +%Y%m%d-%H%M%S)-%(title)s.%(ext)s"
   echo "Done!"
   rm urls.txt
-  zenity --info --text="Audio Extraction Completed!"
+  echo "Audio Extraction Completed!"
 fi
 
 if [ "$ACTION" = "v_playlist" ]; then
   echo ""
   echo "VIDEO PLAYLIST DOWNLOADER"
   InsertURL
-  ProgressFunction yt-dlp -f 'bv*[height=1080]+ba' "$URL" -o 'VideoPlaylist/$(date "%H%M%S")-%(title)s.%(ext)s'
+  DownloadFunction yt-dlp -f 'bv*[height=1080]+ba' "$URL" -o 'VideoPlaylist/%(title)s.%(ext)s'
   echo "Done!"
   rm urls.txt
-  zenity --info --text="Video Playlist Download Completed!"
+  echo "Video Playlist Download Completed!"
 fi
 
 if [ "$ACTION" = "a_playlist" ]; then
   echo ""
   echo "AUDIO PLAYLIST DOWNLOADER"
   InsertURL
-  ProgressFunction yt-dlp --audio-quality "ba" -x "$URL" -o 'AudioPlaylist/$(date +%Y%m%d-%H%M%S)-%(title)s.%(ext)s'
+  DownloadFunction yt-dlp --audio-quality "ba" -x "$URL" -o 'AudioPlaylist/%(title)s.%(ext)s'
   echo "Done!"
   rm urls.txt
-  zenity --info --text="Audio Playlist Download Completed!"
+  echo "Audio Playlist Download Completed!"
 fi
