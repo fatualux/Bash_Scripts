f13cab6b3fb852c49248405828e691824c4315f4
fatualux <35587292+fatualux@users.noreply.github.com>
2024-05-18 21:08:12 +0200

    Removed pull from official repo

--- comfyUI.sh
+++ comfyUI.sh
@@ -6,7 +6,7 @@ WORKDIR=$HOME/.virtualenv/comfyUI
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKDIR/lib/python3.11/site-packages/
 
-cd $WORKDIR && git pull
+cd $WORKDIR
 source $WORKDIR/bin/activate && pip install --upgrade pip
 pip install -r $WORKDIR/requirements.txt
 python $WORKDIR/main.py
