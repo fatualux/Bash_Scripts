c182f5bf72ec8b74a64468b863ed6ab1c16e2f96
fatualux <35587292+fatualux@users.noreply.github.com>
2024-06-02 10:28:02 +0200

    Removed (wrong) description of the script

--- yt_stream.sh
+++ yt_stream.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#This script opens a shell prompt, asks user for an URL and plays it with no sound.
 #It depends on: bash mpv yad yt-dlp
 
-# precompile yad form with clipboard content
 CLIPBOARD=$(wl-paste)
 URL=$(yad --title="YouTube Stream" --form --field="YouTube URL:" "$CLIPBOARD" --width=820 --height=460)
 
