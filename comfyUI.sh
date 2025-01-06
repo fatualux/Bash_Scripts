00dd1a7a8f76295a0b8b57c40056d5c7fe213b91
fatualux <35587292+fatualux@users.noreply.github.com>
2025-01-06 22:32:22 +0100

    Fixed path

--- comfyUI.sh
+++ comfyUI.sh
@@ -2,10 +2,10 @@
 
 clear
 
-WORKDIR=$HOME/.virtualenv/comfyUI
+WORKDIR=$HOME/.virtualenv/comfyui
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$WORKDIR/lib/python3.10/site-packages/
 
 cd $WORKDIR
 source $WORKDIR/.venv/bin/activate && pip install --upgrade pip
-python $WORKDIR/main.py
+python $WORKDIR/ComfyUI/main.py
